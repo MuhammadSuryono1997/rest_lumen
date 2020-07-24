a3f1cb1afc1f2c81467ba91791aafde5b3cf645e
MuhammadSuryono1997 <msuryono0@gmail.com>
2020-07-24 16:53:36 +0900

    create controller product,cutomer,order

--- app/Orders.php
+++ app/Orders.php
@@ -8,18 +8,18 @@ class Orders extends Model
 {
     protected $table = 'orders';
 
-    public function customer()
-    {
-        return $this->belongsTo('App\Customers');
-    }
+    // public function customer()
+    // {
+    //     return $this->belongsTo('App\Customers');
+    // }
 
-    public function order_item()
-    {
-        return $this->hasOne('App\OrderItems', 'foreign_key', 'order_id');
-    }
+    // public function order_item()
+    // {
+    //     return $this->hasOne('App\OrderItems', 'foreign_key', 'order_id');
+    // }
 
-    public function payment()
-    {
-        return $this->hasOne('App\Payments', 'foreign_key', 'order_id');
-    }
+    // public function payment()
+    // {
+    //     return $this->hasOne('App\Payments', 'foreign_key', 'order_id');
+    // }
 }
