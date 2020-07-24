a6f27c04c4a010909e299f2ef5e956cbc0b2b0fc
MuhammadSuryono1997 <msuryono0@gmail.com>
2020-07-24 22:16:03 +0900

    Update Payment

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -71,10 +71,10 @@ class PaymentController extends Controller
         $item_order = $this->get_items(15);
         for($i=0; $i < count($item_order); $i++)
         {
-            $item_list['id'] = $item_order[$i]['id'];
-            $item_list['price'] = $item_order[$i]['product']['price'];
-            $item_list['quantity'] = $item_order[$i]['quantity'];
-            $item_list['name'] = $item_order[$i]['product']['name'];
+            $item_list[]['id'] = $item_order[$i]['id'];
+            $item_list[]['price'] = $item_order[$i]['product']['price'];
+            $item_list[]['quantity'] = $item_order[$i]['quantity'];
+            $item_list[]['name'] = $item_order[$i]['product']['name'];
         }
         // return $item_list;
         $item_new[] = [
